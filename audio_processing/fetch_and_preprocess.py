b24fd3f5f68fc74d52a2686295498dc141db618b
Ubuntu <ubuntu@ip-172-31-7-95.us-west-1.compute.internal>
2021-01-18 18:26:06 +0000

    Improvements on the comment

--- audio_processing/fetch_and_preprocess.py
+++ audio_processing/fetch_and_preprocess.py
@@ -5,39 +5,37 @@ import subprocess
 
 class FetchAndDecompress():
     def __init__(self, bucket_folder):
-        """Fetchs the audio files from the s3 bucket and decompress them
+        """Download audio files from s3 and preprocess them
         
-        Fetchs the files from the s3 Bucket, decompress them, appends them 
-        to a list and converts the format so python can use them
+        Download audio files from audiofilesource S3 bucket, unzip
+        the files and tranform them with the help of FFMPEG
         """
-        self.BUCKET = 'audiofilesource'
-        self.prefix = bucket_folder + '/'
+        self.prefix = bucket_folder
         self.download_files_from_bucket()
         self.unzip_files()
         self.convert_adpcm_to_pcm()
         
     def download_files_from_bucket(self):
-        """"Downloads from a bucket all the files inside a folder.
+        """"Downloads bucket all the files inside a folder.
 
-        List all the objects of a folder, except the zero lenth object with the name
-        of the folder (folder object) created by the S3 Management Console. Loops 
-        through that list and downloads them to the current location.
+        List all the objects inside a folder in the audiofilesource bucket, 
+        except the zero lenth object with the same name of the folder (folder object) 
+        created automatically by the S3 Management Console and downloads them to the current location.
         """
         client = boto3.client('s3')
         zip_files = client.list_objects_v2(
-            Bucket=self.BUCKET, 
+            Bucket='audiofilesource', 
             Prefix=self.prefix, 
             StartAfter=self.prefix)  # Eliminates from the list the zero length object named like the
                                 # folder (folder object) created by the S3 Management Console.
                                 # The folder object it is the first element in the list and its
                                 # named equal to the folder (prefix)
         for zip_file in zip_files['Contents']:
-            client.download_file(self.BUCKET,
+            client.download_file('audiofilesource',
                                  zip_file['Key'],
                                  os.path.basename(zip_file['Key'])) #basename eliminates the prefix
         
     def unzip_files(self):
-        """Extract the zip files and deletes them afterwards"""
         for file in os.listdir(os.getcwd()):
             if file.endswith('.zip'):
                 zip_ref = zipfile.ZipFile(file)
@@ -47,10 +45,11 @@ class FetchAndDecompress():
         
     
     def convert_adpcm_to_pcm(self):
-        """Converts the files with .wav extension from adpcm to pcm
+        """Converts the .wav files from adpcm to pcm
         
-        converts the .wav files from compresion format Adaptive Differential Pulse 
-        Code Modulation (adpcm) to Pulse Code Modulation (pcm) using ffmpeg in a subprocess
+        The .wav files, with a compresion format of Adaptive Differential Pulse 
+        Code Modulation (adpcm) are converted to Pulse Code Modulation (pcm) 
+        using ffmpeg plataform ejecuted in a python subprocess
 
         """
         for file in os.listdir():
