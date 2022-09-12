1096e3e92e4acc2ce594228f144884ab12fe544e
Ubuntu <ubuntu@ip-172-31-10-221.us-west-1.compute.internal>
2022-09-12 21:10:20 +0000

    New data format by day

--- audio_processing/extract_data.py
+++ audio_processing/extract_data.py
@@ -25,8 +25,8 @@ class extractData():
         
         self.SAMPLE_SIZE_IN_SECONDS = 30 
         self.NOISE_THRESHOLD = 0.5 #50% threshold for noise
-        self.GRAPH_THRESHOLD = 0.1 #10% threshold for the graph
         self.rec_datetime = rec_datetime
+        self.day = self.rec_datetime.replace(hour=0, minute=0, second=0)
         self.audio_data = numpy.array([], dtype=numpy.int16)
         self.helpers = helpers
       
@@ -47,9 +47,10 @@ class extractData():
         
     # Extract Samples
     def extract_data(self):
-        graph_data = []
+        graph_data = {}
+        daily_data = []
         last_samples = numpy.array([], dtype=numpy.int16)
-        max_data_value = numpy.iinfo(last_samples.dtype).max
+        max_value = numpy.iinfo(last_samples.dtype).max
         sound_data_generator = self.get_sound_data()
         
         for samplerate, data in sound_data_generator:
@@ -57,22 +58,27 @@ class extractData():
             data_sample_list = self.get_data_samples(samplerate, concatenated_data)
             last_samples = data_sample_list[-1]
             for data_sample in data_sample_list[:-1]:
-                graph_data.append({
-                    'time': self.rec_datetime.isoformat(),
-                    'maxLoudness': float(numpy.amax(data_sample)/max_data_value)
-                })
                 self.rec_datetime += timedelta(seconds=self.SAMPLE_SIZE_IN_SECONDS)
-                
+                self.organize_data_by_day(graph_data, daily_data, data_sample, max_value)
+        
+        graph_data[self.day.isoformat()] = daily_data
         self.helpers.create_JSON(graph_data)
         
+    def organize_data_by_day(self, graph_data, daily_data, data_sample, max_value):
+        if self.rec_datetime > self.day + timedelta(days=1):
+            graph_data[self.day.isoformat()] = daily_data.copy()
+            daily_data.clear()
+            self.day = self.rec_datetime.replace(hour=0, minute=0, second=0)
+        daily_data.append({
+            'time': self.rec_datetime.isoformat(),
+            'maxLoudness': float(numpy.amax(data_sample)/max_value)
+        })
     
     def get_data_samples(self, samplerate, data):
         sample_size = int(self.SAMPLE_SIZE_IN_SECONDS * samplerate)
         samples = numpy.array_split(data, range(sample_size, len(data), sample_size))
         return samples
-
         
-
 # if numpy.amax(recording_data) > numpy.iinfo(recording_data.dtype).max * self.NOISE_THRESHOLD:
 #     mp3_name = datetime.datetime.strftime(self.rec_datetime.date(), '%Y-%m-%d') + '.mp3'
 #     mp3_link = self.helpers.create_mp3_audio_files(samplerate, recording_data, mp3_name)
