a06966cb8cd5ca7566047e86ccbc0958243c3a28
Ubuntu <ubuntu@ip-172-31-27-238.us-west-1.compute.internal>
2023-05-12 19:38:47 +0000

    Refactoring code for trouble shotting

--- audio_processing/entry.py
+++ audio_processing/entry.py
@@ -5,14 +5,14 @@ from extract_data import extractData
 
 
 #The audiofilesource S3 bucket folder were the source files are in (1020-Helm-Ln-Foster-City-Ca-94404/audio/)
-source_folder = '1020-Helm-Ln-Foster-City-Ca-94404/audio/'
+source_folder = '2141-Mills-Ave-Menlo-Park-CA-94025/audio/'
 #The quietavenue.com S3 bucket folder were the generated audio files are going to be stored (1020-Helm-Ln-Foster-City-CA-94404/)
-destination_folder = '1020-Helm-Ln-Foster-City-CA-94404'
+destination_folder = '2141-Mills-Ave-Menlo-Park-CA-94025/'
 #The key of the element in quietavenue DynamoDB table where the link to S3 bucket is going to be stored (1020-Helm-Ln-Foster-City-Ca-94404)
-dynamodb_item_key =	'1020-Helm-Ln-Foster-City-Ca-94404'
-recording_start_datetime = datetime(2020, 2, 11, 10, 00) #YYYY, MM, DD, HH, MM,
+dynamodb_item_key =	'2141-Mills-Ave-Menlo-Park-CA-94025'
+recording_start_datetime = datetime(2022, 3, 6, 19, 34) #YYYY, MM, DD, HH, MM,
 
 new_helper = helpers(source_folder, destination_folder, dynamodb_item_key)
 #fetchAndPreprocess(new_helper)
-extractData(recording_start_datetime, new_helper).extract_data()
+extractData(recording_start_datetime, new_helper)
 #new_helper.clean_folder()
--- audio_processing/extract_data.py
+++ audio_processing/extract_data.py
@@ -25,17 +25,12 @@ class extractData():
         
         self.SAMPLE_SIZE_IN_SECONDS = 300
         self.SAMPLE_SPLIT_SIZE_IN_SECONDS = 3
-        self.NOISE_THRESHOLD = 0.43 #50% threshold for noise
-        self.rec_datetime = rec_datetime
-        self.day = self.rec_datetime.replace(hour=0, minute=0, second=0)
-        self.daily_sound_data = numpy.array([], dtype=numpy.int16)
+        self.NOISE_THRESHOLD = 0.43 #43% threshold for noise
+        self.SECONDS_IN_A_DAY = 24 * 60 *60
+        self.NUMPY_ARRAY_INT16_MAX_VALUE = numpy.iinfo(numpy.array([], dtype=numpy.int16).dtype).max
         self.audio_data = {}
-        self.daily_graph_data = []
-        self.last_sample = numpy.array([], dtype=numpy.int16)
-        self.numpy_int16_max_value = numpy.iinfo(self.last_sample.dtype).max
         self.helpers = helpers
-        self.samplerate = 14400
-        self.sound_start_in_seconds = 0
+        self.extract_data(rec_datetime)
     
     def get_sound_data(self):
         wav_files = self.sort_wave_files()
@@ -52,56 +47,60 @@ class extractData():
         return wav_files
         
     # Extract Samples
-    def extract_data(self):
+    def extract_data(self, current_time):
+        self.current_day = current_time.replace(hour=0, minute=0, second=0)
+        day_duration_in_sec = (self.current_day - current_time).seconds
+        sound_data_array = numpy.array([], dtype=numpy.int16)
         sound_data_generator = self.get_sound_data()
-        
         for self.samplerate, data in sound_data_generator:
-            concatenated_data = numpy.concatenate((self.last_sample, data), axis=None)
-            data_sample_list = self.array_split(concatenated_data, self.SAMPLE_SIZE_IN_SECONDS)
-            last_samples = data_sample_list[-1]
-            for data_sample in data_sample_list[:-1]:
-                self.rec_datetime += timedelta(seconds=self.SAMPLE_SIZE_IN_SECONDS)
-                self.organize_data_by_day(data_sample)
+            sound_data_array = numpy.concatenate((sound_data_array, data))
+            if sound_data_array.size > day_duration_in_sec * self.samplerate:
+                day_data_array = self.array_split(sound_data_array, day_duration_in_sec)
+                self.analize_data(day_data_array[0], current_time)
+                sound_data_array = numpy.array([], dtype=numpy.int16)
+                sound_data_array = numpy.append(sound_data_array, day_data_array[1])
+                day_duration_in_sec = self.SECONDS_IN_A_DAY # The consecutive day after the first have the same duration or the generator finishes
+                current_time = self.current_day = self.current_day + timedelta(hours=24)
         
-        self.store_data()
+        self.analize_data(sound_data_array, current_time)
         self.helpers.create_JSON(self.audio_data)
-        
-    def organize_data_by_day(self, data_sample):
-        if self.rec_datetime > self.day + timedelta(days=1):
-            self.store_data()
-        data_point = {
-            'time': self.rec_datetime.isoformat()
-        }
-        self.analize_data(data_sample, data_point)
-        self.daily_graph_data.append(data_point)
     
-    def store_data(self):
-        mp3_name = datetime.strftime(self.day, '%Y-%m-%d') + '.mp3'
-        mp3_link = self.helpers.create_mp3_audio_files(self.samplerate, self.daily_sound_data, mp3_name)
-        self.audio_data[self.day.isoformat()] = {'mp3Link': mp3_link}
-        self.audio_data[self.day.isoformat()]['graphData'] = self.daily_graph_data.copy()
-        self.daily_graph_data.clear()
-        self.daily_sound_data = numpy.array([], dtype=numpy.int16)
-        self.day = self.rec_datetime.replace(hour=0, minute=0, second=0)
-        self.sound_start_in_seconds = 0
+    def analize_data(self, data_array, start_time):
+        sound_data_array = numpy.array([], dtype=numpy.int16)
+        graph_data_points_array = []
+        data_samples_list = self.array_split(data_array, self.SAMPLE_SIZE_IN_SECONDS)
+        for data_sample in data_samples_list:
+            parts_list = self.array_split(data_sample, self.SAMPLE_SPLIT_SIZE_IN_SECONDS)
+            parts_max_loudness_list, part_audio_data_array = self.filter_sample_audio_data(parts_list)
             
-    def analize_data(self, data_sample, data_point):
-        parts = self.array_split(data_sample, self.SAMPLE_SPLIT_SIZE_IN_SECONDS)
-        maximums = numpy.amax(parts, axis=1)
-        data_point['maxLoudness'] = float(numpy.mean(maximums)/self.numpy_int16_max_value)
-        self.get_sounds_from_samples(parts, data_point)
-        
-    def get_sounds_from_samples(self, parts, data_point):
-        sound_duration_in_seconds =  0
+            data_point = {
+                'maxLoudness': float(numpy.mean(parts_max_loudness_list)/self.NUMPY_ARRAY_INT16_MAX_VALUE),
+                'time': start_time.isoformat()
+            }
+            if part_audio_data_array.size != 0:
+                data_point['soundStart'] = sound_data_array.size/self.samplerate
+            
+            sound_data_array = numpy.append(sound_data_array, part_audio_data_array)
+            graph_data_points_array.append(data_point)
+            start_time += timedelta(seconds=self.SAMPLE_SIZE_IN_SECONDS)
+        self.store_data(sound_data_array, graph_data_points_array)
+    
+    def filter_sample_audio_data(self, parts):
+        sound_data_concentration_array = numpy.array([], dtype=numpy.int16)
+        max_list = []
         for part in parts:
-            if (numpy.amax(part) > self.NOISE_THRESHOLD * self.numpy_int16_max_value):
-                sound_duration_in_seconds += self.SAMPLE_SPLIT_SIZE_IN_SECONDS
-                self.daily_sound_data = numpy.append(self.daily_sound_data, part)
-        if sound_duration_in_seconds:
-            data_point['soundStart'] = self.sound_start_in_seconds
-            data_point['soundEnd'] = self.sound_start_in_seconds + sound_duration_in_seconds
-            self.sound_start_in_seconds += sound_duration_in_seconds
-        
+            part_max = numpy.amax(part)
+            max_list.append(part_max)
+            if (part_max > self.NOISE_THRESHOLD * self.NUMPY_ARRAY_INT16_MAX_VALUE):
+                sound_data_concentration_array = numpy.append(sound_data_concentration_array, part)
+        return max_list, sound_data_concentration_array
+    
+    def store_data(self, audio_data_array, graph_data_points_array):
+        mp3_name = datetime.strftime(self.current_day, '%Y-%m-%d') + '.mp3'
+        mp3_link = self.helpers.create_mp3_audio_files(self.samplerate, audio_data_array, mp3_name)
+        self.audio_data[self.current_day.isoformat()] = {'mp3Link': mp3_link}
+        self.audio_data[self.current_day.isoformat()]['graphData'] = graph_data_points_array.copy()
+    
     def array_split(self, data, split_size_in_seconds):
         split_size = int(split_size_in_seconds * self.samplerate)
         return numpy.array_split(data, range(split_size, len(data), split_size))
