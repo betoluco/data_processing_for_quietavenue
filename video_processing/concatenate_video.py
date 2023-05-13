ac79e6291d617e4749c327b4dc095fb857c61137
Ubuntu <ubuntu@ip-172-31-27-238.us-west-1.compute.internal>
2023-05-13 20:20:03 +0000

    Changes on data processing flow

--- video_processing/concatenate_video.py
+++ video_processing/concatenate_video.py
@@ -1,29 +1,24 @@
 from helpers import helpers
 
-UPLOAD_FOLDER = '1020 Helm Ln Foster City, CA 94404, USA/'
-UPLOAD_BUCKET = 'videofilesource'
-
 RAW_DATA_BUCKET = 'quietavenue-raw-data'
 RAW_DATA_FOLDER = '1020-Helm-Ln-Foster-City-Ca-94404/video/'
 
-CONCATENATED_FILE_NAME = 'concatVideo.mp4'
-UPLOAD_BUCKET = 'videofilesource'
+CONCATENATED_FILE_NAME = '1020-Helm-Ln-Foster-City-Ca-94404Video.mp4'
+UPLOAD_BUCKET = 'quietavenue-dev-s3bucketassets-1k6f7f4u682l1'
+UPLOAD_FOLDER = 'assets/2141-Mills-Ave-Menlo-Park-CA-94025/'
 
 MP4 = '.mp4'
 AVI = '.AVI'
 TS = '.ts'
 
 helpers = helpers()
-helpers.download_files_from_bucket(UPLOAD_FOLDER, UPLOAD_BUCKET)
-avi_files_list = helpers.sort_files(AVI)
-helpers.convert_to_mp4(avi_files_list)
-helpers.clean_folder(AVI)
-mp4_files_lits = helpers.sort_files(MP4)
-helpers.contatenate_video(mp4_files_lits, CONCATENATED_FILE_NAME)
-helpers.upload_file_to_bucket(['concatF.mp4'], RAW_DATA_FOLDER, RAW_DATA_BUCKET)
-mp4_files_lits = helpers.sort_files(MP4)
-helpers.upload_file_to_bucket(mp4_files_lits, RAW_DATA_FOLDER, RAW_DATA_BUCKET)
-helpers.clean_folder((MP4, TS))
+helpers.download_files_from_bucket(RAW_DATA_FOLDER, RAW_DATA_BUCKET)
+#helpers.convert_to_mp4(avi_files_list)
+#helpers.clean_folder(AVI)
+#mp4_files_lits = helpers.sort_files(MP4)
+#helpers.contatenate_video(mp4_files_lits, CONCATENATED_FILE_NAME)
+helpers.upload_file_to_bucket([CONCATENATED_FILE_NAME], UPLOAD_FOLDER, UPLOAD_BUCKET)
+#helpers.clean_folder((MP4, TS))
 
 
 
